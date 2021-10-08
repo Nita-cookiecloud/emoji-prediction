e8334bf3ad0d3c66799a88f87280f4c39a393a81
Nita-cookiecloud <80438015+Nita-cookiecloud@users.noreply.github.com>
2021-10-07 18:44:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)  {
+        document.getElementById("result").innerHTML = '<img id="Captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier ('https://teachablemachine.withgoogle.com/models/vTw0A1euj/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('Captured_image');
+    classifier.classify(img, gotResults);
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first predition is " + prediction_1;
+    speak_data_2 = "And the second preditionis "+prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+         console.log(results);
+         document.getElementById("result_emotion_name").innerHTML = results[0].label;
+         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+         prediction_1 = results[0].label;
+         prediction_2 = results[1].label;
+         speak();
+     if(results[0].label == "happy" )
+    {
+    document.getElementById("update_emoji").innerHTML = "&#128522;";
+    }
+    if(results[0].label == "sad")
+    {
+        documents.getElementById("update_emoji").innerHTML = "&#128532;";
+    }
+    if(results[0].label =="angry")
+    {
+        documents.getElementById("update_emoji").innerHTML  = "&#128548";
+    }
+
+    if(results[0].label == "happy" )
+    {
+     document.getElementById("update_emoji").innerHTML = "&#128522;";
+    } 
+    if(results[1].label == "sad")
+    {
+    documents.getElementById("update_emoji").innerHTML = "&#128532;";
+    }
+    if(results[1].label =="angry")
+   {
+     documents.getElementById("update_emoji").innerHTML  = "&#128548";
+    }
+        
+}}
